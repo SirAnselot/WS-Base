8707bcd04594a930d01aef2dec9e95f328b40963
Ansgar Hiller <ansgar@weigelstein.de>
2016-06-15 22:35:43 +0200

    replaced grunt/configBridge.json w/ grunt/vendorBridge.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,63 +9,66 @@ module.exports = function (grunt) {
   	RegExp.quote = function (string) {
     	return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
   	};
-
   	var fs = require('fs');
   	var path = require('path');
   	var glob = require('glob');
 	var mq4HoverShim = require('mq4-hover-shim');
 	var autoprefixer = require('autoprefixer')({
-		browsers: [
-		  //
-		  // Official browser support policy:
-		  // http://v4-alpha.getbootstrap.com/getting-started/browsers-devices/#supported-browsers
-		  //
-		  'Chrome >= 35', // Exact version number here is kinda arbitrary
-		  // Rather than using Autoprefixer's native "Firefox ESR" version specifier string,
-		  // we deliberately hardcode the number. This is to avoid unwittingly severely breaking the previous ESR in the event that:
-		  // (a) we happen to ship a new Bootstrap release soon after the release of a new ESR,
-		  //     such that folks haven't yet had a reasonable amount of time to upgrade; and
-		  // (b) the new ESR has unprefixed CSS properties/values whose absence would severely break webpages
-		  //     (e.g. `box-sizing`, as opposed to `background: linear-gradient(...)`).
-		  //     Since they've been unprefixed, Autoprefixer will stop prefixing them,
-		  //     thus causing them to not work in the previous ESR (where the prefixes were required).
-		  'Firefox >= 31', // Current Firefox Extended Support Release (ESR)
-		  // Note: Edge versions in Autoprefixer & Can I Use refer to the EdgeHTML rendering engine version,
-		  // NOT the Edge app version shown in Edge's "About" screen.
-		  // For example, at the time of writing, Edge 20 on an up-to-date system uses EdgeHTML 12.
-		  // See also https://github.com/Fyrd/caniuse/issues/1928
-		  'Edge >= 12',
-		  'Explorer >= 9',
-		  // Out of leniency, we prefix these 1 version further back than the official policy.
-		  'iOS >= 8',
-		  'Safari >= 8',
-		  // The following remain NOT officially supported, but we're lenient and include their prefixes to avoid severely breaking in them.
-		  'Android 2.3',
-		  'Android >= 4',
-		  'Opera >= 12'
+		 browsers: [
+			//
+			// Official browser support policy:
+			// http://v4-alpha.getbootstrap.com/getting-started/browsers-devices/#supported-browsers
+			//
+			'Chrome >= 35', // Exact version number here is kinda arbitrary
+			// Rather than using Autoprefixer's native "Firefox ESR" version specifier string,
+			// we deliberately hardcode the number. This is to avoid unwittingly severely breaking the previous ESR in the event that:
+			// (a) we happen to ship a new Bootstrap release soon after the release of a new ESR,
+			//     such that folks haven't yet had a reasonable amount of time to upgrade; and
+			// (b) the new ESR has unprefixed CSS properties/values whose absence would severely break webpages
+			//     (e.g. `box-sizing`, as opposed to `background: linear-gradient(...)`).
+			//     Since they've been unprefixed, Autoprefixer will stop prefixing them,
+			//     thus causing them to not work in the previous ESR (where the prefixes were required).
+			'Firefox >= 31', // Current Firefox Extended Support Release (ESR)
+			// Note: Edge versions in Autoprefixer & Can I Use refer to the EdgeHTML rendering engine version,
+			// NOT the Edge app version shown in Edge's "About" screen.
+			// For example, at the time of writing, Edge 20 on an up-to-date system uses EdgeHTML 12.
+			// See also https://github.com/Fyrd/caniuse/issues/1928
+			'Edge >= 12',
+			'Explorer >= 9',
+			// Out of leniency, we prefix these 1 version further back than the official policy.
+			'iOS >= 8',
+			'Safari >= 8',
+			// The following remain NOT officially supported, but we're lenient and include their prefixes to avoid severely breaking in them.
+			'Android 2.3',
+			'Android >= 4',
+			'Opera >= 12'
 		]
 	});
 
 	// var generateCommonJSModule = require('./grunt/bs-commonjs-generator.js');
-  	var configBridge = grunt.file.readJSON('./grunt/configBridge.json', { encoding: 'utf8' });
-	var vendorlist = '';
-	configBridge.paths.vendor.forEach(function (val, i, arr) {
-		vendorlist += ' * – ' + val.split('vendor/')[1] + '\n';
+	var _package = grunt.file.readJSON('package.json'),
+		 _vendor	 = _package.config.files.js.vendor.src,
+		 _bridge  = grunt.file.readJSON('grunt/vendorBridge.json', { encoding: 'utf8' });
+
+	var _bannerJSList = '';
+	_bridge.paths.vendor.forEach(function (val, i, arr) {
+		_bannerJSList += ' * – ' + val + '\n';
+		arr[i] = path.join(_vendor, val);
 	});
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		banner: '/*!\n' +
+		pkg: _package,
+		banner: 	'/*!\n' +
             	' * VendorJS Compiler v<%= pkg.version %>\n' +
-            	' * Compiled <%= grunt.template.today("yyyy-mm-dd") %> by <%= pkg.author %> (<%= pkg.homepage %>)\n' +
-				' * \n' +
-				' * Included vendors:\n' +
-				' * --------------------------------------------------\n' +
-				vendorlist +
-				' * --------------------------------------------------\n' +
-            	' */\n',
+					' * Compiled <%= grunt.template.today("yyyy-mm-dd") %> by <%= pkg.author %> (<%= pkg.homepage %>)\n' +
+					' * \n' +
+					' * Included vendors:\n' +
+					' * --------------------------------------------------\n' +
+					_bannerJSList +
+					' * --------------------------------------------------\n' +
+					' */\n',
 		clean: {
-			dist: 'public',
+			dist: 'public/dist',
 		},
 
 		// JS build configuration
@@ -99,7 +102,7 @@ module.exports = function (grunt) {
 	        	dest: '<%= pkg.config.files.js.app.dest %>/<%= pkg.config.client %>.js'
 	      	},
 	      	vendor: {
-	        	src:  configBridge.paths.vendor,
+	        	src:  _bridge.paths.vendor,
 	        	dest: '<%= pkg.config.files.js.vendor.dest %>/vendor.js'
 	      	}
 	    },
