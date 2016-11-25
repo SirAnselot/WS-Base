47095dd050d30e68c028a070e4d6d2611349d343
Ansgar Hiller <ansgar@weigelstein.de>
2016-11-25 13:29:18 +0100

    re-init

--- public/js/vendor/vendor.min.js
+++ public/js/vendor/vendor.min.js
@@ -1,6 +1,6 @@
 /*!
  * VendorJS Compiler v1.0.0
- * Compiled 2016-10-26 by Ansgar Hiller <ansgar@weigelstein.de> (http://www.weigelstein.de)
+ * Compiled 2016-11-25 by Ansgar Hiller <ansgar@weigelstein.de> (http://www.weigelstein.de)
  * 
  * Included vendors:
  * --------------------------------------------------
